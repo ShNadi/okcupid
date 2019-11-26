0d763a2d4bfcf13ed09cf79aa06789438710a094
Shiva Nadi <nadi.shiva@gmail.com>
2019-11-26 13:33:23 +0100

    pre_process.py is compeleted(v1) and main.py saves the preprocessed dataframe in data/processed/preprocessed_cupid.csv

--- src/main.py
+++ src/main.py
@@ -2,16 +2,19 @@ import pre_processing
 import pandas as pd
 
 
-cupid_df = pd.read_csv('D:/Github/OkCupid/Data/profiles.csv', usecols = ['education', 'essay0', 'essay1', 'essay2', 'essay3',
-                                                                   'essay7', 'essay8', 'essay9', 'age', 'sex'])
+cupid_df = pd.read_csv('../data/processed/preprocessed_cupid.csv')
+# cupid_df = pd.read_csv('D:/Github/OkCupid/Data/profiles.csv', usecols = ['education', 'essay0', 'essay1', 'essay2', 'essay3',
+#                                                                    'essay7', 'essay8', 'essay9', 'age', 'sex'])
 
 
 
-
-cupid = pre_processing.PreProcess()
-cupid_df = cupid.missing_value(cupid_df)
-cupid_df = cupid.merge_essay(cupid_df)
-cupid_df = cupid.remove_tag(cupid_df)
-cupid_df = cupid.count_words_sentences(cupid_df)
-
+#
+# cupid = pre_processing.PreProcess()
+# cupid_df = cupid.missing_value(cupid_df)
+# cupid_df = cupid.merge_essay(cupid_df)
+# cupid_df = cupid.remove_tag(cupid_df)
+# cupid_df = cupid.recode_edcuaction(cupid_df)
+# cupid_df = cupid.count_words_sentences(cupid_df)
+# cupid_df = cupid.text_cleaning(cupid_df)
+# export_csv = cupid_df.to_csv (r'../data/processed/preprocessed_cupid.csv', index = None, header=True)
 print(cupid_df)
--- src/pre_processing.py
+++ src/pre_processing.py
@@ -156,6 +156,7 @@ class PreProcess():
         df['isced'] = df['education']
         df = df.replace(dict(isced=dic))
         df.isced[df['isced'] == '-1'] = "None"
+        print(colored('education column is coded...', 'green'))
 
         return df
 
@@ -177,6 +178,7 @@ class PreProcess():
 
         # Remove numbers
         df.removed_stopwords = df.removed_stopwords.str.replace('\d+', '')
+        print(colored('text is clean...', 'green'))
         return df
 
 
